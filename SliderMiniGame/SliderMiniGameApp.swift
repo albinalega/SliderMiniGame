348944d360bbd8b5df5d6ca977ae27471f080a19
albinalega <lega.albina@mail.ru>
2022-12-19 17:35:38 +0400

    Initial Commit

---
+++ SliderMiniGame/SliderMiniGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderMiniGameApp.swift
+//  SliderMiniGame
+//
+//  Created by Альбина Лега on 19.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderMiniGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
