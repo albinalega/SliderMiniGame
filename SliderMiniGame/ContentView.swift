0d48ebd1d212583835b8b03fb4bad11f51d3baf1
albinalega <lega.albina@mail.ru>
2022-12-19 18:22:35 +0400

    made an interface

--- SliderMiniGame/ContentView.swift
+++ SliderMiniGame/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderValue = 50.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("Продвиньте слайдер как можно ближе к:")
+                Text("50")
+            }
+            .padding(.bottom, 20)
+            
+            SliderView(value: $sliderValue)
+            
+            ButtonView(title: "Проверь меня!", action: {})
+                .padding()
+            
+            ButtonView(title: "Начать заново", action: {})
+            
         }
         .padding()
+    }
+}
+
+struct SliderView: View {
+    @Binding var value: Double
+    
+    var body: some View {
+        HStack {
+            Text("0")
+            Slider(value: $value, in: 0...100)
+            Text("100")
+        }
+    }
+}
+
+struct ButtonView: View {
+    
+    let title: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+        }
     }
 }
 
